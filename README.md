c768997d5cae043e83cff5b2e5d3f5acb1a39705
Brandon Hilkert <brandon@meeteor.com>
2012-01-16 20:29:00 -0500

    Update README and documentation.x

--- README.md
+++ README.md
@@ -11,11 +11,42 @@ Usage
 `gem install egg_carton`
 
 
-
 Helpers
 -------
 
+The helper methods use a pre-calculate numberator and denominator with the goal of calculating an average or conversion. 
+In many cases, counts are pulled from a database and then need to be displayed properly for reporting. These methods help with 
+the display portion once the values have been calculated.
 
+In Rails, helper methods are added to `ActionView::Base`, and as a result, available to views.
+
+__Average Method__
+
+`average(1, 4)`
+
+`# => 0.25`
+
+`average(1, 4, 1)`
+
+`# => 0.3`
+
+__Conversion Method__
+
+`conversion(1, 4, 2)`
+
+`# => "25%"`
+
+`conversion(1, 4, 2, :percentage => false)`
+
+`# => 0.25`
+
+`conversion(1, 4, 1)`
+
+`# => "30%"`
+
+`conversion(1, 4, 1, :percentage => false)`
+
+`# => 0.3`
   
 Released under the [MIT license](http://www.opensource.org/licenses/mit-license.php).
 
--- lib/egg_carton/helper.rb
+++ lib/egg_carton/helper.rb
@@ -1,10 +1,49 @@
 module EggCarton
+
   module Helper
+
+    # Public: Calculates average for a numerator and denominator.
+    #
+    # numberator  - The Integer or Float to use as the numberator.
+    # denominator - The Integer or Float to use as the denominator.
+    # precision - The Integer or decimal places to preserve.
+    #
+    # Examples
+    #
+    #   average(1, 4, 2)
+    #   # => 0.25
+    #
+    #   average(1, 4, 1)
+    #   # => 0.3
+    #
+    # Returns the Float result of the calculation
     def average(numerator, denominator, precision = 0)
       return 0 if denominator.zero?
       (numerator.to_f / denominator.to_f).round(precision)
     end
 
+    # Public: Calculates conversion for a numerator and denominator.
+    #
+    # numberator  - The Integer or Float to use as the numberator.
+    # denominator - The Integer or Float to use as the denominator.
+    # precision - The Integer or decimal places to preserve.
+    # opts - The options Hash
+    #
+    # Examples
+    #
+    #   conversion(1, 4, 2)
+    #   # => "25%"
+    #
+    #   conversion(1, 4, 2, :percentage => false)
+    #   # => "0.25"
+    #
+    #   conversion(1, 4, 1)
+    #   # => "30%"
+    #
+    #   conversion(1, 4, 1, :percentage => false)
+    #   # => "0.3"
+    #
+    # Returns the Float result of the calculation
     def conversion(numerator, denominator, precision = 0, opts={})
       return 0 if denominator.zero?
       as_percentage = opts[:percentage].nil? ? true : opts[:percentage]
